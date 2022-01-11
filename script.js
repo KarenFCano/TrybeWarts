92a2540aa25768b3994ce4e8471cc3113b4d4f14
Natã A.Rodrigues <nata26ar@gmail.com>
2022-01-11 20:37:08 -0300

    Requisito 14 Feito, organizaçao e detalhes corrigidos

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-const button = document.getElementById('btn');
+/* const button = document.getElementById('btn');
 
 button.addEventListener('click', function alert(){
-   alert("Email ou senha inválidos");
-});
+  alert("Email ou senha inválidos");
+}); */
+
+// CRIAR RADIO BUTTONS - REQUISITO 14
+function createRadioBtn() {
+  const divRadioBtns = document.getElementById('satisfaction');
+  for (let key = 0; key < 10; key += 1) {
+    const radioBtn = document.createElement('input');
+    radioBtn.type = 'radio';
+    radioBtn.name = 'rate';
+    radioBtn.value = `${key + 1}`;
+    const radioBtnText = document.createElement('p');
+    radioBtnText.innerText = `${key + 1}`;
+    divRadioBtns.appendChild(radioBtnText);
+    divRadioBtns.appendChild(radioBtn);
+  }
+}
+
+createRadioBtn();
